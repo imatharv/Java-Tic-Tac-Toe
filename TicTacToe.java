a18c2db37b302594e4b4698ba213175854406aad
Atharva Joshi <iamatharvaj@google.com>
2021-05-10 10:46:22 +0530

    Added: Solution for displaying the board

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,33 +2,32 @@ import java.util.*;
 import java.lang.*;
 
 public class TicTacToe {
-    private static char[] createBoard() {
-        char[] board = new char[10];
+    public static char[] createBoard() {
+        char[] board = new char[9];
         for(int i=0; i<board.length; i++) {
             board[i] = ' ';
         }
         return board;
     }
-    public void chooseLetter() {
-        String computerLetter, playerLetter;
-        Scanner scanner = new Scanner(System.in);
+    public static char chooseUserLetter(Scanner userInput) {
         System.out.println("Select your letter (X/O): ");
-        playerLetter = scanner.nextLine();
-
-        if(playerLetter.equals("X")) { 
-            computerLetter = "O";
-            System.out.println("Computer letter: "+computerLetter);
-        } else if(playerLetter.equals("O")) {
-            computerLetter = "X";
-            System.out.println("Computer letter: "+computerLetter);
-        } else {
-            System.out.println("Incorrect input");
-        }
+        return userInput.next().toUpperCase().charAt(0);
+    }
+    public static void showBoard(char[] board) {
+        System.out.println(board[0] + " | " + board[1] + " | " + board[2]);
+        System.out.println("-------------");
+        System.out.println(board[3] + " | " + board[4] + " | " + board[5]);
+        System.out.println("-------------");
+        System.out.println(board[6] + " | " + board[7] + " | " + board[8]);
     }
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe Game");
+        Scanner userInput = new Scanner(System.in);
         TicTacToe object = new TicTacToe();
-        char[] board = object.createBoard();
-        object.chooseLetter();
+        char[] board = createBoard();
+        char userLetter = chooseUserLetter(userInput);
+        char computerLetter = (userLetter == 'X') ? 'O' : 'X';
+        System.out.println("Computer letter: "+computerLetter);
+        showBoard(board);
     }
 }
