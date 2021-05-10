5ce20a18e2fd98875c60bf41a9bfe208b247e359
Atharva Joshi <iamatharvaj@google.com>
2021-05-10 12:02:48 +0530

    Added: Solution for taking position from user

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,7 +3,7 @@ import java.lang.*;
 
 public class TicTacToe {
     public static char[] createBoard() {
-        char[] board = new char[9];
+        char[] board = new char[10];
         for(int i=0; i<board.length; i++) {
             board[i] = ' ';
         }
@@ -14,11 +14,31 @@ public class TicTacToe {
         return userInput.next().toUpperCase().charAt(0);
     }
     public static void showBoard(char[] board) {
-        System.out.println(board[0] + " | " + board[1] + " | " + board[2]);
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
         System.out.println("-------------");
-        System.out.println(board[3] + " | " + board[4] + " | " + board[5]);
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
         System.out.println("-------------");
-        System.out.println(board[6] + " | " + board[7] + " | " + board[8]);
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+    }
+    public static void playerMove(char[] board, char userLetter) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the position you want to move to (must be betwween 1 to 9): ");
+        int position = scanner.nextInt();
+
+        if(position >= 1 && position <= 9 && board[position] == ' ') {
+            board[position] = userLetter;
+        }
+        else if(position >= 1 && position <= 9 && board[position] != ' ') {
+            System.out.println("Position is already taken, enter another position: ");
+            position = scanner.nextInt();
+            if(position >= 1 && position <= 9 && board[position] == ' ') {
+                board[position] = userLetter;
+            }
+        }
+        else {
+        	System.out.println("Invalid position.");
+        }	
+        showBoard(board);
     }
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe Game");
@@ -29,5 +49,6 @@ public class TicTacToe {
         char computerLetter = (userLetter == 'X') ? 'O' : 'X';
         System.out.println("Computer letter: "+computerLetter);
         showBoard(board);
+        playerMove(board, userLetter);
     }
 }
