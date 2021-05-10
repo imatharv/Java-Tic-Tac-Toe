0494cdaf079d5fc9a53c4bada0185f47ee6104fd
Atharva Joshi <iamatharvaj@google.com>
2021-05-10 09:32:21 +0530

    Refactored: Solution for creating a board

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,26 +2,16 @@ import java.util.*;
 import java.lang.*;
 
 public class TicTacToe {
-    public static char[][] board;
-    public char[][] createBoard() {
-		for(int i=1; i<=3; i++) {
-            for(int j=1; j<=3; j++) {
-                board[i][j] = '-';
-            }
+    private static char[] createBoard() {
+        char[] board = new char[10];
+        for(int i=0; i<board.length; i++) {
+            board[i] = ' ';
         }
         return board;
-    }
-    public void showBoard() {
-        for(int i=1; i<=3; i++) {
-            for(int j=1; j<=3; j++) {
-                System.out.println(board[i][j] + " | ");
-            }
-        }
     }
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe Game");
         TicTacToe object = new TicTacToe();
-        object.createBoard();
-        object.showBoard();
+        char[] board = object.createBoard();
     }
 }
