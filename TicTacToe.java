c998f8075d2e8a45d5850ea13d9d0acf74df84e0
Atharva Joshi <iamatharvaj@google.com>
2021-05-10 10:01:37 +0530

    Added: Solution for choosing the letters

--- TicTacToe.java
+++ TicTacToe.java
@@ -9,9 +9,26 @@ public class TicTacToe {
         }
         return board;
     }
+    public void chooseLetter() {
+        String computerLetter, playerLetter;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select your letter (X/O): ");
+        playerLetter = scanner.nextLine();
+
+        if(playerLetter.equals("X")) { 
+            computerLetter = "O";
+            System.out.println("Computer letter: "+computerLetter);
+        } else if(playerLetter.equals("O")) {
+            computerLetter = "X";
+            System.out.println("Computer letter: "+computerLetter);
+        } else {
+            System.out.println("Incorrect input");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe Game");
         TicTacToe object = new TicTacToe();
         char[] board = object.createBoard();
+        object.chooseLetter();
     }
 }
